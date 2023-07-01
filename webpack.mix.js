836ea307fe8f4becd520178d38f21923d23caa7a
Martin <darras.martin@gmail.com>
2023-07-01 14:37:43 +0200

    init project + webpack fix

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,61 +1,58 @@
-let mix = require('laravel-mix');
-require('laravel-mix-purgecss');
+let mix = require("laravel-mix");
 
-mix.setPublicPath('./');
+mix.setPublicPath("./");
 
-const CopyWebpackPlugin  = require('copy-webpack-plugin');
+const CopyWebpackPlugin = require("copy-webpack-plugin");
 
-mix.extract(['lodash'])
-    .js('resources/scripts/main.js', 'public/assets/scripts')
-    .disableNotifications()
-    .version();
+mix
+  .extract(["lodash"])
+  .js("resources/scripts/main.js", "public/assets/scripts")
+  .disableNotifications()
+  .version();
 
-mix.sass('resources/styles/app.scss', 'public/assets/styles')
-    .options({processCssUrls: true})
-    .purgeCss({
-        content: [`public/*.html`],
-        whitelistPatterns: [/hljs/],
-    })
-    .disableNotifications();
+mix
+  .sass("resources/styles/app.scss", "public/assets/styles")
+  .options({ processCssUrls: true })
+  .disableNotifications();
 
 mix.webpackConfig({
-    module: {
-        rules: [
-            {
-                test: /\.(scss|sass|css)$/i,
-                enforce: "pre",
-                use: [
-                    {
-                        loader: "import-glob-loader",
-                    }
-                ]
+  module: {
+    rules: [
+      {
+        test: /\.(scss|sass|css)$/i,
+        enforce: "pre",
+        use: [
+          {
+            loader: "import-glob-loader",
+          },
+        ],
+      },
+      {
+        test: /.js/,
+        exclude: [/node_modules\/(?!(dom7)\/).*/, /\.test\?$/],
+        enforce: "pre",
+        use: [
+          {
+            loader: "import-glob-loader",
+          },
+          {
+            loader: "babel-loader",
+            options: {
+              presets: ["@babel/preset-env"],
             },
-            {
-                test: /.js/,
-                exclude: [/node_modules\/(?!(dom7)\/).*/, /\.test\?$/],
-                enforce: "pre",
-                use: [
-                    {
-                        loader: "import-glob-loader",
-                    },
-                    {
-                        loader: 'babel-loader',
-                        options: {
-                            presets: ['@babel/preset-env']
-                        }
-                    }
-                ]
-            }
-        ]
-    },
-    plugins: [
+          },
+        ],
+      },
+    ],
+  },
+  /* plugins: [
         new CopyWebpackPlugin([
             {
                 from: __dirname + '/resources/styles/fonts/',
                 to: __dirname + `/public/assets/styles/fonts/`
             }
         ]),
-    ]
+    ] */
 });
 
 mix.version();
