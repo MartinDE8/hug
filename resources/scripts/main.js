b15c1ee5cf957a170c2620c7d1f303054825d986
Martin <darras.martin@gmail.com>
2023-07-12 19:36:19 +0200

    Homepage resp & desk

--- resources/scripts/main.js
+++
@@ -1,9 +0,0 @@
-let hamburger = document.querySelector(".hamburger");
-//let navResp = document.getElementById("navResp");
-
-if (hamburger) {
-  hamburger.addEventListener("click", function () {
-    hamburger.classList.toggle("active");
-    //navResp.classList.toggle("active");
-  });
-}
